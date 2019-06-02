b2235b1a4ee7d1510f753e7183b8e72f51b58b62
SnesarevMS <snesarevms@sibintek.ru>
2019-06-02 22:08:58 +0300

    cleanup

--- src/blocks/generator.py
+++ src/blocks/generator.py
@@ -1,10 +1,26 @@
+class Ship(object):
+    def __init__(self):
+        import random
+
+        self.cargo = {
+            0: 1,
+            1: 1,
+            2: 2
+        }
+        self.size = random.randint(0, 10)
+
+        # stats
+        self.time_in_queue = 0
+        self.time_in_movement = 0
+        self.time_in_unloading = 0
+
+
 class Generator(object):
     def __init__(self, params):
         pass
 
     def next(self):
         from random import triangular
-        from . import Ship
 
         ships = []
         ship_num = int(triangular(0, 3, 0))
